d0baeb5e6dd0ac0f9fa946cd589d2480ef61048e
Xiao Di <xiaodi@Xiaos-MacBook-Air.local>
2026-04-07 17:04:46 +0800

    Add Adsterra popunder ad code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="user-role" content="{{ optional(Auth::user())->is_admin ? 'admin' : 'user' }}">
 
+    <script src="https://pl29084850.profitablecpmratenetwork.com/24/fb/c6/24fbc6c3855b45418eb9f70f4c00a60f.js"></script>
+
+
+
     <!-- Viewport -->
     <meta name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover">
